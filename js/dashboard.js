a8b6ee968a9ab209bdbc209ce172f574d3c12039
Yash Patel <tatva.dotnet.yashpatel@outlook.com>
2024-01-12 18:21:29 +0530

    Responsive sidebar done

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,12 +1,3 @@
-const actualBtn = document.getElementById('actual-btn');
-console.log(actualBtn);
-const fileChosen = document.getElementById('file-chosen');
-
-actualBtn.addEventListener('change', function() {
-    fileChosen.textContent = this.files[0].name;
-    fileChosen.style.color = "black";
-    fileChosen.style.fontSize = "large"
-})
 var sidebar = document.getElementById('sidebar');
 console.log(sidebar);
 var flag = 0;
